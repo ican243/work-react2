a0935bbf391b8b6c861730599ebc47f3dc3d7016
ican243 <ican243@gmail.com>
2026-05-08 17:40:51 +0900

    2026.05.08

--- project3/src/pages/Home.jsx
+++ project3/src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { DiaryStateContext } from "../App";
 import Button from "../component/Button";
 import Header from "../component/Header";
+import { getMonthRangeByDate } from "../util";
+import DiaryList from "../component/DiaryList";
 
 const Home = () => {
   const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
+  const [filteredData, setFilteredData] = useState([]);
   const headerTitle = `${pivotDate.getFullYear()}년 ${pivotDate.getMonth() +1}월`;
   const onIncreaseMonth = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1))
@@ -15,6 +18,19 @@ const Home = () => {
     setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1));
   };
 
+  useEffect(() => {
+    if (data.length >= 1) {
+      const { beginTimeStamp, endTimeStamp } = getMonthRangeByDate(pivotDate);
+      setFilteredData(
+        data.filter(
+          (it) => beginTimeStamp <= it.date && it.date <= endTimeStamp
+        )
+      );
+    } else {
+      setFilteredData([]);
+    }
+  }, [data, pivotDate]);
+
   return (
     <div>
     <Header
@@ -22,6 +38,7 @@ const Home = () => {
     leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
     rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
     />
+    <DiaryList data={filteredData} />
     </div>
 
   ); 
